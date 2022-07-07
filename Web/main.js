b5b50049a4337f79f77c54addf7344dc1bfd49f2
moas1974 <memaa1974@gmail.com>
2022-07-07 22:15:39 +0200

    toilet list

--- Web/main.js
+++ Web/main.js
@@ -1,4 +1,5 @@
 let map;
+let toiletList;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -27,6 +28,20 @@ function error(err) {
 }
 
 async function showPosition(position) {
+  drawCurrentLocation(position);
+
+  const listToiletEndpoint = new URL("http://localhost:3000/toilet");
+  const response = await fetch(listToiletEndpoint);
+  toiletList = await response.json();
+  let markers = drawMarkers();
+
+  const markerCluster = new markerClusterer.MarkerClusterer({
+    map,
+    markers,
+  });
+}
+
+function drawCurrentLocation(position) {
   let mapPosition = {
     lat: position.coords.latitude,
     lng: position.coords.longitude,
@@ -45,12 +60,10 @@ async function showPosition(position) {
       strokeColor: "#ffffff",
     },
   });
+}
 
-  const listToiletEndpoint = new URL("http://localhost:3000/toilet");
-  const response = await fetch(listToiletEndpoint);
-  const toiletList = await response.json();
+function drawMarkers() {
   let markers = [];
-
   toiletList.forEach((element) => {
     let marker = new google.maps.Marker({
       position: {
@@ -62,9 +75,55 @@ async function showPosition(position) {
     });
     markers.push(marker);
   });
-
-  const markerCluster = new markerClusterer.MarkerClusterer({
-    map,
-    markers,
-  });
+  return markers;
 }
+
+function showToiletList() {
+  let cardHTML = "";
+  let numCallbackRuns = 0;
+
+  toiletList.forEach((element) => {
+    let toiletType;
+    if (element.isOwnedByWall === "Yes") {
+      toiletType = "public toilet";
+    } else {
+      toiletType = "private toilet";
+    }
+
+    let toiletPrice;
+    if (element.Price === "0,00") {
+      toiletPrice = "Free";
+    } else {
+      toiletPrice = `Paid ${element.Price} euro`;
+    }
+
+    cardHTML += `
+  <div class="card" onclick="showToiletDetalis(${numCallbackRuns})">
+  <div class="card-horizontal">
+    <div class="img-square-wrapper">
+      <img
+        class=""
+        src="http://via.placeholder.com/300x180"
+        alt="toilet image"
+      />
+    </div>
+    <div class="card-body">
+      <h4 class="card-title">${element.Description}</h4>
+      <p class="card-text">
+        ${toiletType}
+        ${toiletPrice}
+      </p>
+    </div>
+  </div>
+</div>`;
+    numCallbackRuns++;
+  });
+  document.getElementById("list-wrapper").innerHTML = cardHTML;
+}
+
+function showToiletDetalis(index) {
+  alert(`you clicked on ${toiletList[index].Description}`);
+}
+
+window.showToiletList = showToiletList;
+window.showToiletDetalis = showToiletDetalis;
