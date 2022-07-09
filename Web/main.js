52199e31413693ed81acf529ddb07d25e44ace18
moas1974 <memaa1974@gmail.com>
2022-07-09 20:50:24 +0200

    list js & html completed

--- Web/main.js
+++ Web/main.js
@@ -12,8 +12,6 @@ function initMap() {
   getLocation();
 }
 
-window.initMap = initMap;
-
 function getLocation() {
   if (navigator.geolocation) {
     let options = {
@@ -35,7 +33,7 @@ async function showPosition(position) {
   toiletList = await response.json();
   let markers = drawMarkers();
 
-  const markerCluster = new markerClusterer.MarkerClusterer({
+  new markerClusterer.MarkerClusterer({
     map,
     markers,
   });
@@ -50,7 +48,7 @@ function drawCurrentLocation(position) {
   new google.maps.Marker({
     position: mapPosition,
     map,
-    title: "Hello World!",
+    title: "you are here!",
     icon: {
       path: google.maps.SymbolPath.CIRCLE,
       scale: 10,
@@ -79,69 +77,174 @@ function drawMarkers() {
 }
 
 function showToiletList() {
-  alert("showList");
   let closeListIcon = document.getElementById("close-list");
   closeListIcon.classList.remove("hidden");
   closeListIcon.classList.add("active-icon");
 
-  let cardHTML = "<div id='cards-wrapper'>";
-  let numCallbackRuns = 0;
+  let listButton = document.getElementById("list-button");
+  listButton.classList.add("active-map-button");
+
+  let cardHTML = "<div id='cards-wrapper' class='scrollbar scrollbar-primary'>";
 
   toiletList.forEach((element) => {
-    let toiletType;
     if (element.isOwnedByWall === "Yes") {
-      toiletType = "public toilet";
+      element.toiletType = "public toilet";
     } else {
-      toiletType = "private toilet";
+      element.toiletType = "private toilet";
     }
 
-    let toiletPrice;
     if (element.Price === "0,00") {
-      toiletPrice = "Free";
+      element.toiletPrice = "Free";
     } else {
-      toiletPrice = `Paid ${element.Price} euro`;
+      element.toiletPrice = `Paid ${element.Price} euro`;
     }
 
     cardHTML += `
-  <div class="card" onclick="showToiletDetalis(${numCallbackRuns})">
-  <div class="card-horizontal">
-    <div class="img-square-wrapper">
-      <img
-        class=""
-        src="http://via.placeholder.com/300x180"
-        alt="toilet image"
-      />
-    </div>
-    <div class="card-body">
-      <h4 class="card-title">${element.Description}</h4>
-      <p class="card-text">
-        ${toiletType}
-        ${toiletPrice}
-      </p>
-    </div>
-  </div>
-</div>`;
-    numCallbackRuns++;
+        <div class="card" onclick='showToiletDetalis(${JSON.stringify(
+          element
+        )})'>
+        <div class="card-horizontal">
+          <div class="img-square-wrapper">
+            <img
+              class=""
+              src="http://via.placeholder.com/300x180"
+              alt="toilet image"
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">${element.Description}</h4>
+            <p class="card-text">
+              ${element.toiletType}
+              ${element.toiletPrice}
+            </p>
+          </div>
+        </div>
+      </div>`;
   });
 
   cardHTML += "</div>";
   document.getElementById("list-wrapper").innerHTML = cardHTML;
 }
 
-function showToiletDetalis(index) {
-  alert(`you clicked on ${toiletList[index].Description}`);
+function showToiletDetalis(toilet) {
+  document.body.innerHTML += `
+  <div id="toilet-details" class="card">
+    <i class="fa-solid fa-xmark fa-2x" onclick="closeToiletDetails()"></i>
+    <img class="card-img-top" src="http://via.placeholder.com/300x180" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${toilet.Description}</h5>
+      <p class="card-text">${toilet.Street}</p>
+      <div>
+        <span class="badge badge-pill">${toilet.toiletPrice}</span>
+        <span class="badge badge-pill">${toilet.toiletType}</span>
+      </div>
+      <a href="#" class="btn map-button" onclick='showToiletReviews(${JSON.stringify(
+        toilet
+      )})'>Show reviews</a>
+    </div>
+  </div>
+  `;
+}
+
+function showToiletReviews(toilet) {
+  let detailsCard = document.getElementById("toilet-details");
+  detailsCard.classList.add("hidden");
+
+  document.body.innerHTML += `
+  <div id="toilet-reviews" class="card">
+    <div class="card-icons">
+      <i class="fa-solid fa-arrow-left fa-2x" onclick='goBack("toilet-reviews", "toilet-details")'></i>
+      <i class="fa-solid fa-xmark fa-2x" onclick="closeToiletReviews()"></i>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Reviews</h5>
+      <p class="card-text">${toilet.reviews || "No reviews."}</p>
+      <a href="#" class="btn map-button" onclick='showAddReviewCard(${JSON.stringify(
+        toilet._id
+      )})'>Add review</a>
+    </div>
+  </div>
+  `;
+}
+
+function showAddReviewCard(id) {
+  let reviewsCard = document.getElementById("toilet-reviews");
+  reviewsCard.classList.add("hidden");
+
+  document.body.innerHTML += `
+  <div id="toilet-add-review" class="card">
+    <div class="card-icons">
+      <i class="fa-solid fa-arrow-left fa-2x" onclick='goBack("toilet-add-review", "toilet-reviews")'></i>
+      <i class="fa-solid fa-xmark fa-2x" onclick="closeAddReviewCard()"></i>
+    </div>
+    <div class="card-body">
+      <form id="reviewForm">
+        <textarea name="reviewText" class="reviewTextBox"></textarea><br>
+        <input class="btn map-button" type="button" onclick='submitReview(this.form, ${JSON.stringify(
+          id
+        )})' value="Add Review">
+      </form>
+    </div>
+  </div>
+  `;
+}
+
+function submitReview(form, id) {
+  let reviewsCard = document.getElementById("toilet-add-review");
+  reviewsCard.classList.add("hidden");
+
+  // call backend here to add comment
+
+  closeAddReviewCard();
 }
 
 function closeList() {
-  alert("closeList");
   let closeListIcon = document.getElementById("close-list");
   closeListIcon.classList.remove("active-icon");
   closeListIcon.classList.add("hidden");
 
   let cardsWrapper = document.getElementById("cards-wrapper");
   cardsWrapper.classList.add("hidden");
+
+  let listButton = document.getElementById("list-button");
+  listButton.classList.remove("active-map-button");
+  listButton.blur();
 }
 
+function closeToiletDetails() {
+  let toiletDetalis = document.getElementById("toilet-details");
+  if (toiletDetalis != null) document.body.removeChild(toiletDetalis);
+}
+
+function closeToiletReviews() {
+  closeToiletDetails();
+  let toiletReviews = document.getElementById("toilet-reviews");
+  if (toiletReviews != null) document.body.removeChild(toiletReviews);
+}
+
+function closeAddReviewCard() {
+  closeToiletReviews();
+  closeToiletDetails();
+  let toiletAddReview = document.getElementById("toilet-add-review");
+  if (toiletAddReview != null) document.body.removeChild(toiletAddReview);
+}
+
+function goBack(from, to) {
+  let fromElement = document.getElementById(from);
+  if (fromElement != null) document.body.removeChild(fromElement);
+
+  let toElement = document.getElementById(to);
+  toElement.classList.remove("hidden");
+}
+
+window.initMap = initMap;
 window.showToiletList = showToiletList;
 window.showToiletDetalis = showToiletDetalis;
 window.closeList = closeList;
+window.closeToiletDetails = closeToiletDetails;
+window.showToiletReviews = showToiletReviews;
+window.closeToiletReviews = closeToiletReviews;
+window.submitReview = submitReview;
+window.showAddReviewCard = showAddReviewCard;
+window.closeAddReviewCard = closeAddReviewCard;
+window.goBack = goBack;
