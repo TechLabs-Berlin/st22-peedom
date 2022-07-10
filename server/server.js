a656cab01690771d192abebd0a6cf026930bb689
Noah Baumann <noah.baumann@yahoo.de>
2022-07-10 19:42:55 +0200

    testing out new endpoints and removed bug from server.js file

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@
 
 // require express and bodyParser
 const  express = require("express");
-const cors = require('cors');
-const fs = require("fs");
 const  bodyParser = require("body-parser");
+const fs = require("fs")
+const cors = require("cors")
+const axios = require("axios")
 
 // create express app
 const  app = express();
@@ -15,22 +16,11 @@ const  port = process.env.PORT || 3000;
 // use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(express.json)
-
+app.use(cors());
 
 // Add endpoint
 app.get('/', (req, res) => {
-res.send(await fs.readFile("st22-peedom\Web\map.html", "utf-8"));
-});
-// Add endpoint for user location
-app.post('/location', (req, res) => {
-    console.log(request.body);
-    const data = request.body;
-    response.json({
-        latitude: data.lat,
-        longitutude: data.lng
-    });
+  res.send("Hello World");
   });
 
 // Listen to server
@@ -44,5 +34,4 @@ require("./config/db");
 
 // Import API route
 var routes = require('./api/routes/toiletRoutes'); //importing route
-const { response } = require("express");
 routes(app);
