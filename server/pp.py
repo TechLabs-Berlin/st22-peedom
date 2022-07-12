99aee178cebd733cc18c4b8f041571044bcbb1e9
Noah Baumann <noah.baumann@yahoo.de>
2022-07-12 21:50:05 +0200

    basic functionality for calling all toilet data

--- server/pp.py
+++ server/pp.py
@@ -1,26 +1,25 @@
 import sys
 import json
 import requests
-from flask import Flask, request
 
-@app.route("/toilet", methods=["GET"])
-def search():
-    args = request.args
-    lon = args.get("lon")
-    lat = args.get("lat")
-
-
-url = "http://3000/toilets"
-
-r = requests.get(url, params=location)
-
+#API with all toilets 
+url = "http://localhost:3000/toilets-all"
+#making get request to get all toilets
+r = requests.get(url)
+#inputing all toilets Information into data variable
 data = r.json()
 
-resp = {
-    "Response": 200,
-    "Message": "hello from Python File",
-    "Data":data
-}
-print(json.dumps(resp))
-# using system module in python to send to node js 
-sys.stdout.flush()
+# lat = sys.argv[1]
+# lon = sys.argv[2]
+print(data)
+
+# print(data)
+
+# resp = {
+#     "Response": 200,
+#     "Message": "hello from Python File",
+#     "Data":data
+# }
+# print(json.dumps(resp))
+# # using system module in python to send to node js 
+# sys.stdout.flush()
