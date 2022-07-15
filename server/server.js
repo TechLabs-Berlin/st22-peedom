581c590a3b040cc35a3871a4d25fde39ccb00b66
Noah Baumann <noah.baumann@yahoo.de>
2022-07-15 11:40:32 +0200

    query params, i.e. location and filters are passed to python file as string

--- server/server.js
+++ server/server.js
@@ -3,25 +3,11 @@
 // require express and bodyParser
 const  express = require("express");
 const  bodyParser = require("body-parser");
-const fs = require("fs")
 const cors = require("cors")
-// const axios = require("axios")
 
 // create express app
 const  app = express();
 
-// const spawn = require("child_process").spawn;
-// const childPython = spawn("python", ["--version"])
-// const childPython = spawn("python", ["pp.py"])
-// childPython.stdout.on("data", (data) => {
-//     console.log(`stdout: ${data}`);
-// })
-// childPython.stderr.on("data", (data) => {
-//     console.log(`stderr: ${data}`);
-// })
-// childPython.on("close", (code) => {
-//     console.log(`exited with: ${code}`);
-// })
 // define port to run express app
 const  port = process.env.PORT || 3000;
 
@@ -30,59 +16,29 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// let runPy = new Promise(function(success, nosuccess) {
-
-//   const { spawn } = require('child_process');
-//   const pyprog = spawn('python', ['./../pypy.py']);
-
-//   pyprog.stdout.on('data', function(data) {
-
-//       success(data);
-//   });
-
-//   pyprog.stderr.on('data', (data) => {
-
-//       nosuccess(data);
-//   });
-// });
-
 // Add endpoint
 app.get('/toilet', (req, res) => {
     let dataTest = ""
-    //for (const [key, value] of mySearchParams.entries()) {}
+    // turn query parameters into string that will be passed to pp.py
+    let paramString = JSON.stringify(req.query)
+
     const spawn = require("child_process").spawn;
     // const childPython = spawn("python", ["../../../server/pp.py", req.query.lat, req.query.lng]);
-    const childPython = spawn("python", ["pp.py"]);
+    const childPython = spawn("python", ["pp.py", paramString]);
     childPython.stdout.on("data", (data) => {
         const dataToSend = data.toString()
         dataTest += dataToSend
     })
     childPython.stdout.on("close", (code) => {
-        console.log("Hello");
+        // console.log("Hello");
         //convert dataTest to JSON
         let myData = JSON.parse(dataTest)
         //extract Data form Object
+        console.log(myData.Message);
         res.status(200).json(myData.Data)
     })
   });
-//   childPython.stdout.on("data", (err, data) => {
-//     console.log(data);
-    // if (data) {
 
-    //     // const dataToSend = data.toString()
-    //     // testData += dataToSend
-    // }
-    // if (err) {
-    //     res.status(500).send(err);
-    //     return
-    // }
-    // console.log("Hello")
-    // res.send(data);
-    // return
-
-// childPython.stdout.on("close", (code) => {
-//     res.send(testData)
-// })
 // Listen to server
 app.listen(port, () => {
 
