26ffd5394a3827f256a6210365558375ee2136a4
Noah Baumann <noah.baumann@yahoo.de>
2022-07-20 12:26:25 +0200

    added axios to make request to flask server within node /toilet endpoint, had to change main.js because it was making a request to /toilets-all instead of /toilet

--- Web/main.js
+++ Web/main.js
@@ -30,7 +30,14 @@ function error(err) {
 
 async function showPosition(position) {
   drawCurrentLocation(position);
-  await callAPIAndUpdateMap("");
+
+  let queryParams = ``;
+
+  appliedFilters.forEach((element) => {
+    queryParams += `&${element}=true`;
+  });
+
+  await callAPIAndUpdateMap(queryParams);
 }
 
 async function callAPIAndUpdateMap(queryParams) {
@@ -100,13 +107,8 @@ function drawMarkers() {
 }
 
 function redirectToGogleMaps(toilet) {
-  let toGoogleMapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${
-    userCurrentPosition.lat
-  }
-  ,${userCurrentPosition.lng}&destination=${+toilet.Latitude.replace(
-    ",",
-    "."
-  )},${+toilet.Longitude.replace(",", ".")}`;
+  let toGoogleMapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${userCurrentPosition.lat}
+  ,${userCurrentPosition.lng}&destination=${toilet.Latitude},${toilet.Longitude}`;
 
   window.open(toGoogleMapsUrl, "_blank").focus();
 }
@@ -129,8 +131,8 @@ function showToiletList() {
         <div class="card-horizontal">
           <div class="img-square-wrapper">
             <img
-              class=""
-              src="http://via.placeholder.com/300x180"
+              class="list-image"
+              src="images/toilet1.svg"
               alt="toilet image"
             />
           </div>
@@ -155,20 +157,20 @@ function showToiletDetalis(toilet) {
   toiletDetailsWrapper.innerHTML += `
   <div id="toilet-details" class="card">
     <i class="fa-solid fa-xmark fa-2x" onclick="closeToiletDetails()"></i>
-    <img class="card-img-top" src="http://via.placeholder.com/300x180" alt="Card image cap">
+    <img class="card-img-top" src="images/toilet3.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${toilet.Description}</h5>
-      <p class="card-text">${toilet.address} ${toilet.PostalCode}</p>
-      <div>
+      <p class="card-text">${toilet.street_address} ${toilet.PostalCode}</p>
+      <div class="badge-pill-info">
         <span class="badge badge-pill">${toilet.toiletPrice}</span>
         <span class="badge badge-pill">${toilet.timings} hr</span>
       </div>
-      <a href="#" class="btn map-button" onclick='showToiletReviews(${JSON.stringify(
+      <a href="#" class="btn map-button details-card-button" onclick='showToiletReviews(${JSON.stringify(
         toilet
       )})'>Show reviews</a>
-      <a href="#" class="btn map-button" onclick='redirectToGogleMaps(${JSON.stringify(
+      <a href="#" class="btn map-button details-card-button" onclick='redirectToGogleMaps(${JSON.stringify(
         toilet
-      )})'>Diection</a>
+      )})'>Directions</a>
     </div>
   </div>
   `;
@@ -188,7 +190,7 @@ function showToiletReviews(toilet) {
     <div class="card-body">
       <h5 class="card-title">Reviews</h5>
       <p class="card-text">${toilet.reviews || "No reviews."}</p>
-      <a href="#" class="btn map-button" onclick='showAddReviewCard(${JSON.stringify(
+      <a href="#" class="btn map-button submit-button" onclick='showAddReviewCard(${JSON.stringify(
         toilet._id
       )})'>Add review</a>
     </div>
@@ -210,7 +212,7 @@ function showAddReviewCard(id) {
     <div class="card-body">
       <form id="reviewForm" action="/toilet/${toilet.id}" method="POST">
         <textarea name="reviewText" class="reviewTextBox"></textarea><br>
-        <input class="btn map-button" type="button" onclick='submitReview(this.form, ${JSON.stringify(
+        <input class="btn map-button addReviw-button" type="button" onclick='submitReview(this.form, ${JSON.stringify(
           id
         )})' value="Add Review">
       </form>
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 const  express = require("express");
 const  bodyParser = require("body-parser");
 const cors = require("cors")
+const axios = require('axios');
 
 // create express app
 const  app = express();
@@ -16,28 +17,64 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// Add endpoint
-app.get('/toilet', (req, res) => {
-    let dataTest = ""
-    // turn query parameters into string that will be passed to pp.py
-    let paramString = JSON.stringify(req.query)
+// endpoint to update with comment
+// app.patch("/toilet/:id/edit", async (req, res) =>
+//     const = 
+// )
 
-    const spawn = require("child_process").spawn;
-    // const childPython = spawn("python", ["../../../server/pp.py", req.query.lat, req.query.lng]);
-    const childPython = spawn("python", ["pp.py", paramString]);
-    childPython.stdout.on("data", (data) => {
-        const dataToSend = data.toString()
-        dataTest += dataToSend
+// Add endpoint
+
+app.get("/toilet", (req, res) => {
+    const userParams = req.query
+    axios.get('http://127.0.0.1:5000/toilets', 
+    {params: userParams})
+  .then((response) => {
+    res.send(response.data)
+    // console.log(response.status);
+    // console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  })
+  .catch(err => {
+    // console.log(err);
+    axios.get("http://localhost:3000/toilets-all")
+    .then((response) => {
+        res.send(response.data)
     })
-    childPython.stdout.on("close", (code) => {
-        // console.log("Hello");
-        //convert dataTest to JSON
-        let myData = JSON.parse(dataTest)
-        //extract Data form Object
-        console.log(myData.Message);
-        res.status(200).json(myData.Data)
-    })
-  });
+  })
+
+})
+
+// app.patch("/toilet", (req, res) => {
+//     Toilet.findOneAndUpdate({ _id:req.params.id }, req.body, { new:true }, (err, toilet) => {
+//     if (err) {
+//     res.status(500).send(err);
+//     }
+//     res.status(200).json(toilet);
+//     });
+//     });
+
+// app.get('/toilet', (req, res) => {
+//     let dataTest = ""
+//     // turn query parameters into string that will be passed to pp.py
+//     let paramString = JSON.stringify(req.query)
+
+//     const spawn = require("child_process").spawn;
+//     // const childPython = spawn("python", ["../../../server/pp.py", req.query.lat, req.query.lng]);
+//     const childPython = spawn("python", ["pp.py", paramString]);
+//     childPython.stdout.on("data", (data) => {
+//         const dataToSend = data.toString()
+//         dataTest += dataToSend
+//     })
+//     childPython.stdout.on("close", (code) => {
+//         // console.log("Hello");
+//         //convert dataTest to JSON
+//         let myData = JSON.parse(dataTest);
+//         //extract Data form Object
+//         console.log(myData.Message);
+//         res.status(200).json(myData.Data)
+//     })
+//   });
 
 // Listen to server
 app.listen(port, () => {
